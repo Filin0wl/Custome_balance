35985edb709ebe3e58970ca506a71583f90b891e
Stacy <nastia-dogf@mail.ru>
2020-05-31 21:08:23 +0300

    Load project

---
+++ Custome_balance/Custome_property.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Custome_balance
+{
+    public class Custome_property
+    {
+        public bool VisibleLabel { get; set; }
+        public Color ColorItem { get; set; }
+        public int PositionRowText{ get; set; }
+        public int PositionRowMoney{ get; set; }
+        public int Positioncolomn{ get; set; }
+
+        public Custome_property(bool vis, Color col)
+        {
+            VisibleLabel = vis;
+            ColorItem = col;
+            if(vis)
+            {
+                PositionRowText = 3;
+                PositionRowMoney = 4;
+                Positioncolomn = 0;
+                
+            }
+            else
+            {
+                PositionRowText = 1;
+                PositionRowMoney = 2;
+                Positioncolomn = 1;
+            }
+        }
+    }
+}
---
+++ Custome_balance/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Custome_balance
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        bool flag = false;
+        public MainPage()
+        {
+            BindingContext = new Custome_property(flag, Color.FromHex("#e73895"));
+            InitializeComponent();
+        }
+
+        private void TapFrame(object sender, EventArgs e)
+        {
+            
+            BindingContext = new Custome_property(!flag, Color.FromHex("#e73895"));
+            flag = !flag;
+
+        }
+    }
+}
